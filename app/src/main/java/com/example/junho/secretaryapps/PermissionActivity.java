502c475ba6aa868ec235ce32bb2a127970ebb6f2
junho <chlwnsgh311@gmail.com>
2018-10-28 16:27:25 +0900

    second commit

---
+++ app/src/main/java/com/example/junho/secretaryapps/PermissionActivity.java
@@ -0,0 +1,21 @@
+package com.example.junho.secretaryapps;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class PermissionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+
+
+
+    }
+}
